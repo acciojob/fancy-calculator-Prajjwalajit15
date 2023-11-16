b090d900a37f0c8186ee8c3c04ccf78950c5aed0
prajjwal ajit <122018880+Prajjwalajit15@users.noreply.github.com>
2023-11-16 14:11:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+function m(p1,p2) {
+	return (p1*2)+p2;
+}
+
+let n1 = parseInt(prompt("1st input"));
+let n2 = parseInt(prompt("2nd input"));
+
+ alert(m(n1,n2));
+ 

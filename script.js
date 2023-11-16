ddc693a27e6eb4eba62ec906c6c817f5713538a3
prajjwal ajit <122018880+Prajjwalajit15@users.noreply.github.com>
2023-11-16 14:14:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-function m(p1,p2) {
-	return (p1*2)+p2;
+function calculateResult(input1, input2) {
+    // Convert the inputs to integers using parseInt
+    let num1 = parseInt(input1);
+    let num2 = parseInt(input2);
+
+    // Check if the conversion was successful
+    if (!isNaN(num1) && !isNaN(num2)) {
+        // Perform the calculations
+        let result = (num1 * 2) + num2;
+
+        // Display the result using alert
+        alert("The final result is: " + result);
+    } else {
+        // Handle invalid input
+        alert("Invalid input. Please enter valid integers.");
+    }
 }
 
-let n1 = parseInt(prompt("1st input"));
-let n2 = parseInt(prompt("2nd input"));
+let n1 = prompt("1st input");
+let n2 = prompt("2nd input");
 
- alert(m(n1,n2));
+calculateResult(n1, n2);
+
  
